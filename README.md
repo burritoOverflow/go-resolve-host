6ff30ce9b4d39fab3cd86340e7a299b2d2d2e5b2
burritoOverflow <47379530+burritoOverflow@users.noreply.github.com>
2024-09-11 22:16:00 -0400

    Init.

---
+++ README.md
@@ -0,0 +1,6 @@
+Resolves hostnames (and reverse lookup) for n hostnames via the DNS server IP address provided.
+
+```bash
+go build
+./resolve-hostname <dns-server-ip-addr> <hostname1> <hostname2> ...
+```
---
+++ main.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+	"os"
+	"strings"
+	"sync"
+	"time"
+)
+
+type Resolver struct {
+	resolver *net.Resolver
+}
+
+func (r *Resolver) resolveHostname(ctx context.Context, hostname string) {
+	startTime := time.Now()
+
+	ips, err := r.resolver.LookupIP(ctx, "ip4", hostname)
+	if err != nil {
+		log.Printf("Failed to resolve %s: %v\n", hostname, err)
+		return
+	}
+
+	log.Printf("IP addresses for %s: %v\n", hostname, addrString(ips))
+
+	for _, ip := range ips {
+		names, err := r.resolver.LookupAddr(ctx, ip.String())
+		if err != nil {
+			log.Printf("Error performing reverse lookup for %s (%s): %v", ip, hostname, err)
+		} else {
+			log.Printf("Reverse for %s (%s): %v", ip, hostname, strings.Join(names, ", "))
+		}
+	}
+
+	duration := time.Since(startTime)
+	log.Printf("Duration for resolving %s: %d ms\n", hostname, duration.Milliseconds())
+}
+
+func validIpAddr(address string) bool {
+	ip := net.ParseIP(address)
+	return ip != nil
+}
+
+// Use an alternate dialer  provided via`dnsServerAddr` string,
+// specified without the port (53)
+// instead of the default DNS server's address
+func newResolver(dnsServerAddr string) Resolver {
+	r := Resolver{}
+	r.resolver = &net.Resolver{
+		PreferGo:     true, // 'false' results in using the default (network's) DNS server, avoiding lookups via the IP address provided
+		StrictErrors: true,
+		Dial: func(ctx context.Context, network, address string) (net.Conn, error) {
+			d := net.Dialer{Timeout: time.Second * 5}
+			return d.DialContext(ctx, "udp", dnsServerAddr+":53")
+		},
+	}
+	return r
+}
+
+func addrString(ips []net.IP) string {
+	addrStr := ""
+	for i, ip := range ips {
+		if i == len(ips)-1 {
+			addrStr += ip.String()
+		} else {
+			addrStr += ip.String() + ", "
+		}
+	}
+	return addrStr
+}
+
+func main() {
+	totalStart := time.Now()
+
+	if len(os.Args) < 3 {
+		log.Println("Usage: resolve-hostname <dns-server-ip-addr> <hostname1> <hostname2> ...")
+		return
+	}
+
+	dnsServerIp := os.Args[1]
+	if !validIpAddr(dnsServerIp) {
+		log.Fatalf("Invalid ip address: %s", dnsServerIp)
+		os.Exit(1)
+	}
+
+	hostnames := os.Args[2:]
+
+	var wg sync.WaitGroup
+	ctx := context.Background()
+
+	r := newResolver(dnsServerIp)
+
+	for _, hostname := range hostnames {
+		wg.Add(1)
+		go func() {
+			r.resolveHostname(ctx, hostname)
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+
+	totalDuration := time.Since(totalStart)
+	addrs := strings.Join(os.Args[2:], ", ")
+	log.Printf("Total duration for %d addresses: %s: %d ms\n", len(os.Args[2:]), addrs, totalDuration.Milliseconds())
+}
